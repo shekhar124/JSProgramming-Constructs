ac23a5c816b73e634850a9ae70e22494a63bac5d
rajashekhar <rajashekharpasikanti@gmail.com>
2021-10-06 20:53:45 +0530

    sequencial problems

---
+++ sequcincal/RandomOneDigitNumber.js
@@ -0,0 +1,2 @@
+let number =Math.floor(Math.random() * 10);
+console.log("The Single Digit Number Is " +number);
---
+++ sequcincal/sumodDice.js
@@ -0,0 +1,5 @@
+let dice1 = Math.floor(((Math.random() * 10) % 6) + 1);
+let dice2 = Math.floor(((Math.random() * 10) % 6) + 1);
+let sumofdice = dice1+dice2;
+
+console.log(sumofdice);
